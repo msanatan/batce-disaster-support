74fdd13a48a8840f10ad3f279b29da165fcaa0f6
Marcus Sanatan <marcus.sanatan@wepala.com>
2018-10-23 00:16:11 -0400

    Fix class references and structure of header component

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,22 +1,27 @@
-import React from "react";
+import React from 'react';
 import Appbar from 'muicss/lib/react/appbar';
+import Container from 'muicss/lib/react/container';
 import containerStyles from './header.module.css';
 
 const Header = () => {
   return (
     <Appbar className={containerStyles.header}>
-      <table>
-        <tr class="mui--appbar-height">
-          <td class="mui--text-title">BATCE Disaster Support</td>
-          <td class="mui--text-right">
-            <ul class="mui-list--inline mui--text-body2">
-              <li><a href="#">About</a></li>
-              <li><a href="#">Donate</a></li>
-              <li><a href="#">Contact</a></li>
-            </ul>
-          </td>
-        </tr>
-      </table>
+      <Container>
+        <table>
+          <tbody>
+            <tr className='mui--appbar-height'>
+              <td className='mui--text-title'>BATCE Disaster Support</td>
+              <td className='mui--text-right'>
+                <ul className='mui-list--inline mui--text-body2'>
+                  <li><a href="#">About</a></li>
+                  <li><a href="#">Donate</a></li>
+                  <li><a href="#">Contact</a></li>
+                </ul>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </Container>
     </Appbar>
   );
 };
