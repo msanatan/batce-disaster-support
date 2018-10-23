5516bccaee29c6c6fcc8636bdb25bd9443fde68d
Marcus Sanatan <marcus.sanatan@wepala.com>
2018-10-23 00:16:51 -0400

    Created a container for all pages

--- src/components/pageContainer/index.js
+++ src/components/pageContainer/index.js
@@ -1,2 +1,11 @@
-import React from "react";
-import Container from "muicss/lib/react/container";
+import React from 'react';
+import Container from 'muicss/lib/react/container';
+import containerStyles from './pageContainer.module.css';
+
+const PageContainer = (props) => {
+  return (
+    <Container className={containerStyles.wrapper}>{props.children}</Container>
+  );
+};
+
+export default PageContainer;
