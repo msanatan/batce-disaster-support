5b491ad533c425eaef8431b4205d4e5f98c7c29b
Marcus Sanatan <marcus.sanatan@wepala.com>
2018-10-23 08:00:49 -0400

    Cards show the amount of victims first and foremost

--- src/components/statCards/index.js
+++ src/components/statCards/index.js
@@ -2,14 +2,55 @@ import React from 'react';
 import Container from 'muicss/lib/react/container';
 import Row from 'muicss/lib/react/row';
 import Col from 'muicss/lib/react/col';
+import Divider from 'muicss/lib/react/divider';
+import containerStyles from './statCards.module.css';
 
 const StatCards = () => {
   return (
     <Container fluid={true}>
       <Row>
-        <Col md="4">Card</Col>
-        <Col md="4">Card</Col>
-        <Col md="4">Card</Col>
+        <Col md="6">
+          <div className={containerStyles.card + ' mui--z1'}>
+            <Row>
+              <div className={containerStyles.label}>
+                <p className={containerStyles.victimCount}>20</p>
+              </div>
+            </Row>
+            <Divider />
+          </div>
+        </Col>
+        <Col md="6">
+          <div className={containerStyles.card + ' mui--z1'}>
+              <Row>
+                <div className={containerStyles.label}>
+                  <p className={containerStyles.victimCount}>6</p>
+                </div>
+              </Row>
+              <Divider />
+            </div>
+        </Col>
+      </Row>
+      <Row>
+        <Col md="6">
+          <div className={containerStyles.card + ' mui--z1'}>
+              <Row>
+                <div className={containerStyles.label}>
+                  <p className={containerStyles.victimCount}>3</p>
+                </div>
+              </Row>
+              <Divider />
+            </div>
+        </Col>
+        <Col md="6">
+          <div className={containerStyles.card + ' mui--z1'}>
+              <Row>
+                <div className={containerStyles.label}>
+                  <p className={containerStyles.victimCount}>6</p>
+                </div>
+              </Row>
+              <Divider />
+            </div>
+        </Col>
       </Row>
     </Container>
   );
