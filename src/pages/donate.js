6885c4c30d10ddef9d29d2425b942e314b914f93
Marcus Sanatan <marcus.sanatan@wepala.com>
2018-10-23 00:45:25 -0400

    Switch to new boilerplate for future development

--- src/pages/donate.js
+++ src/pages/donate.js
@@ -1,25 +1,10 @@
 import React from 'react';
-import Container from 'muicss/lib/react/container';
-import Button from 'muicss/lib/react/button';
 import PageContainer from '../components/pageContainer';
-import containerStyles from '../components/pageContainer/pageContainer.module.css';
 
 export default () => {
   return (
     <PageContainer>
-      <Container className={containerStyles.wrapper + ' mui--text-center'}>
-        <Container className='mui--appbar-height' />
-        <Container>
-          <br />
-          <br />
-          <Container className={'mui--text-display3'}>
-            Donate
-          </Container>
-          <br/>
-          <br/>
-          <Button  variant='raised' color='accent'>Donate</Button>
-        </Container>
-      </Container>
+      Donate
     </PageContainer>
   );
 };
