0df4b324f49777403ab2c6fadbad486fd3fcdb11
Marcus Sanatan <marcus.sanatan@wepala.com>
2018-10-23 00:22:48 -0400

    Add placeholder pages for content

---
+++ src/pages/contact.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import Container from 'muicss/lib/react/container';
+import Button from 'muicss/lib/react/button';
+import PageContainer from '../components/pageContainer';
+import containerStyles from '../components/pageContainer/pageContainer.module.css';
+
+export default () => {
+  return (
+    <PageContainer>
+      <Container className={containerStyles.wrapper + ' mui--text-center'}>
+        <Container className='mui--appbar-height' />
+        <Container>
+          <br />
+          <br />
+          <Container className={'mui--text-display3'}>
+            Contact
+          </Container>
+          <br/>
+          <br/>
+          <Button  variant='raised' color='accent'>Donate</Button>
+        </Container>
+      </Container>
+    </PageContainer>
+  );
+};
