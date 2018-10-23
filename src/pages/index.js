a7d9f1b6b1b6eea6274757e32d1b4f46ea70a190
Marcus Sanatan <marcus.sanatan@wepala.com>
2018-10-23 00:17:29 -0400

    Use page container instead

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,28 @@
-import React from "react";
-import Header from "../components/header/index";
-import Container from "muicss/lib/react/container";
-import Button from "muicss/lib/react/button";
-import Row from "muicss/lib/react/row";
+import React from 'react';
+import Header from '../components/header/index';
+import Container from 'muicss/lib/react/container';
+import Button from 'muicss/lib/react/button';
+import Row from 'muicss/lib/react/row';
+import PageContainer from '../components/pageContainer';
+import containerStyles from '../components/pageContainer/pageContainer.module.css';
 
 export default () => {
   return (
-    <Container fluid={true}>
-      <Row>
-        <Header/>
-      </Row>
-      <Row>
-        <Button  variant="raised" color="accent">Donate</Button>
-      </Row>
-    </Container>
+    <PageContainer>
+      <Header/>
+      <Container className={containerStyles.wrapper + ' mui--text-center'}>
+        <Container className='mui--appbar-height' />
+        <Container>
+          <br />
+          <br />
+          <Container className={'mui--text-display3'}>
+            Brand.io
+          </Container>
+          <br/>
+          <br/>
+          <Button  variant='raised' color='accent'>Donate</Button>
+        </Container>
+      </Container>
+    </PageContainer>
   );
 };
