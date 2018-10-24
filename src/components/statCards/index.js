a51460300dd5255b96e322b60075166305f42c9b
Marcus Sanatan <msanatan@gmail.com>
2018-10-23 22:17:58 -0400

    Use the modular StatCard component

--- src/components/statCards/index.js
+++ src/components/statCards/index.js
@@ -1,101 +1,34 @@
 import React from 'react';
 import Container from 'muicss/lib/react/container';
 import Row from 'muicss/lib/react/row';
-import Col from 'muicss/lib/react/col';
-import Divider from 'muicss/lib/react/divider';
-import CountUp from 'react-countup';
-import containerStyles from './statCards.module.css';
+import StatCard from '../statCard/index';
 
 const StatCards = () => {
   return (
     <Container fluid={true}>
       <Row>
-        <Col md="6">
-          <div className={containerStyles.card + ' mui--z1'}>
-            <Row>
-              <div className={containerStyles.label}>
-                <CountUp
-                  className={containerStyles.victimCount}
-                  end={20}
-                  delay={0}
-                  duration={1}
-                  useEasing={false}
-                  />
-              </div>
-            </Row>
-            <Divider />
-            <Row>
-              <div className='mui--text-dark-secondary mui--text-subhead'>
-                <p>Victims of severe flooding are BAHSE students</p>
-              </div>
-            </Row>
-          </div>
-        </Col>
-        <Col md="6">
-          <div className={containerStyles.card + ' mui--z1'}>
-              <Row>
-                <div className={containerStyles.label}>
-                <CountUp
-                  className={containerStyles.victimCount}
-                  end={6}
-                  delay={0}
-                  duration={1}
-                  useEasing={false}
-                  />
-                </div>
-              </Row>
-              <Divider />
-              <Row>
-              <div className='mui--text-dark-secondary mui--text-subhead'>
-                <p>Victims of severe flooding are TCE students</p>
-              </div>
-            </Row>
-            </div>
-        </Col>
+        <StatCard
+          count={20}
+          duration={1}
+          description={'BAHSE students are victims of severe flooding'}
+          />
+        <StatCard
+          count={6}
+          duration={1}
+          description={'TCE students are victims of severe flooding'}
+          />
       </Row>
       <Row>
-        <Col md="6">
-          <div className={containerStyles.card + ' mui--z1'}>
-              <Row>
-                <div className={containerStyles.label}>
-                <CountUp
-                  className={containerStyles.victimCount}
-                  end={3}
-                  delay={0}
-                  duration={1}
-                  useEasing={false}
-                  />
-                </div>
-              </Row>
-              <Divider />
-              <Row>
-              <div className='mui--text-dark-secondary mui--text-subhead'>
-                <p>Victims of severe flooding are BATCE 6th Form students</p>
-              </div>
-            </Row>
-            </div>
-        </Col>
-        <Col md="6">
-          <div className={containerStyles.card + ' mui--z1'}>
-              <Row>
-                <div className={containerStyles.label}>
-                <CountUp
-                  className={containerStyles.victimCount}
-                  end={6}
-                  delay={0}
-                  duration={1}
-                  useEasing={false}
-                  />
-                </div>
-              </Row>
-              <Divider />
-              <Row>
-              <div className='mui--text-dark-secondary mui--text-subhead'>
-                <p>Victims of severe flooding are staff</p>
-              </div>
-            </Row>
-            </div>
-        </Col>
+        <StatCard
+          count={3}
+          duration={1}
+          description={'BATCE 6th Form students are victims of severe flooding'}
+          />
+        <StatCard
+          count={6}
+          duration={1}
+          description={'Teachers and staff are victims of severe flooding'}
+          />
       </Row>
     </Container>
   );
